57f2f9392bc4cd7b589702c3181e9a0192d17656
bing <david.bingham@nimbasolutions.com>
2025-01-20 16:53:00 -0700

    add logging

--- tasks/set_password_task.py
+++ tasks/set_password_task.py
@@ -2,10 +2,14 @@ from cumulusci.tasks.apex.anon import AnonymousApexTask
 
 class SetPasswordTask(AnonymousApexTask):
     def _run_task(self):
+        # Log the initial param1 value
+        self.logger.info(f"Initial param1 value: {self.options['param1']}")
+        
         # Run the parent task's _run_task to execute the Apex
         super()._run_task()
         
-        # Store the password value so it can be referenced by other tasks
+        # Store the password value and log it
         self.return_values = {
             "password": self.options["param1"]
-        } 
+        }
+        self.logger.info(f"Stored password in return_values: {self.return_values['password']}") 
